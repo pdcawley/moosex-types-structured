d7168e242c415c178eff87a8aca4aca275034f24
john napiorkowski <jjn1056@john-napiorkowskis-computer.local>
2009-08-17 10:22:56 -0400

    changed makefile and bumped version in prep for release

--- lib/MooseX/Types/Structured.pm
+++ lib/MooseX/Types/Structured.pm
@@ -10,7 +10,7 @@ use Sub::Exporter -setup => { exports => [ qw(Dict Tuple Optional slurpy) ] };
 use Devel::PartialDump;
 use Scalar::Util qw(blessed);
 
-our $VERSION = '0.17';
+our $VERSION = '0.18';
 our $AUTHORITY = 'cpan:JJNAPIORK';
 
 =head1 NAME
